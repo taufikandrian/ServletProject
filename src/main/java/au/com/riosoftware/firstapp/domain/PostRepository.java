691d488d38ba3bf64d6d69755dd2acc2618556f5
taufikandrian <MohammadTaufik.Andrian@mitrais.com>
2017-09-12 09:57:10 +0800

    new Feature Thymeleaf

---
+++ src/main/java/au/com/riosoftware/firstapp/domain/PostRepository.java
@@ -0,0 +1,7 @@
+package au.com.riosoftware.firstapp.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+}
